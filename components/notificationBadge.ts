3f8e0ad03f0d1512667537f3e2ccf9ce1d0b1d62
Thor Schueler <tschueler@infusion.com>
2017-03-13 12:30:13 -0500

    Minor bug fixes.

--- components/notificationBadge.ts
+++ components/notificationBadge.ts
@@ -16,18 +16,15 @@ import { Message } from '../models/message';
                     No new notifications
                 </li>
                 <ng-container *ngIf="Messages != null && Messages.length > 0">
-                    <li *ngFor="let m of Messages" class="message">
-                        {{m.Title}}
-                    </li>
                     <ul class="list-group notifications">
                         <ng-container *ngFor="let m of _messages">
                             <a *ngIf="m.Detail == null || m.Detail == ''">
-                                <li class="list-group-item">
+                                <li class="list-group-item message">
                                     {{m.Title}}
                                 </li>
                             </a>
                             <a *ngIf="m.Detail != null && m.Detail != ''" [routerLink]=[m.Detail]>
-                                <li class="list-group-item">
+                                <li class="list-group-item message">
                                     {{m.Title}}
                                 </li>
                             </a>
@@ -45,6 +42,7 @@ import { Message } from '../models/message';
         .dropdown-menu { padding: 0; border: 0; border-radius: 0px; animation-duration: 0.4s; -webkit-animation-duration: 0.4s; z-index: -1; position: absolute; }
         .dropdown-menu .notifications.list-group {list-style: none; padding: 0;margin: 0; }
         .dropdown-menu .notifications.list-group .list-group-item { min-width: 175px; padding: 8px; border: 0; border-bottom: 1px solid #EEE; }
+        .dropdown-menu .notifications.list-group > a .list-group-item { cursor: default }
         .dropdown-menu .notifications.list-group > a[href] .list-group-item { cursor: pointer !important; }
         .dropdown-menu .notifications.list-group > a[href] .list-group-item:hover { background-color: rgba(100,100,100,0.1); }
         .dropdown-menu .title { font-family: 'Roboto Condensed', sans-serif; padding: 5px 10px; -moz-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3); -webkit-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3); box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3); color: #444}
