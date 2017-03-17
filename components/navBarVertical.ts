95bb7c11e2552ea022cd7779ee206ae455d16cbd
Thor Schueler <tschueler@infusion.com>
2017-03-17 10:30:57 -0500

    Tweak current nav styles for multiline nodes.

--- components/navBarVertical.ts
+++ components/navBarVertical.ts
@@ -8,14 +8,14 @@ import { NavNode } from '../models/navNode';
         <ul class="nav navbar-nav">
             <ng-container *ngIf="_root != null">
                 <ng-container *ngFor="let n of _root.Children">
-                    <li *ngIf="n.Children.length==0" class="" [routerLinkActive]="['active']">
+                    <li *ngIf="n.Children.length==0" class="{{n.Title.length > 25 ? 'multiline' : ''}}" [routerLinkActive]="['active']">
                         <a [routerLink]="[n.Url]" id="{{n.Id}}">
-                            <span class="icon fa {{n.FAIcon}}"></span><span class="title {{n.Title.length > 25 ? 'multiline' : ''}} ">{{n.Title}}</span>
+                            <span class="icon fa {{n.FAIcon}} {{n.Title.length > 25 ? 'multiline' : ''}}"></span><span class="title {{n.Title.length > 25 ? 'multiline' : ''}}">{{n.Title}}</span>
                         </a>
                     </li>
-                    <li *ngIf="n.Children.length>0" class="panel panel-default dropdown" [routerLinkActive]="['active']">
+                    <li *ngIf="n.Children.length>0" class="panel panel-default dropdown {{n.Title.length > 25 ? 'multiline' : ''}}" [routerLinkActive]="['active']">
                         <a data-toggle="collapse" id="{{n.Id}}" href="#{{n.Id}}_dropdown">
-                            <span class="icon fa {{n.FAIcon}}"></span><span class="title">{{n.Title}}</span>
+                            <span class="icon fa {{n.FAIcon}} {{n.Title.length > 25 ? 'multiline' : ''}}"></span><span class="title {{n.Title.length > 25 ? 'multiline' : ''}}">{{n.Title}}</span>
                         </a>
                         <!-- Dropdown -->
                         <div id="{{n.Id}}_dropdown" class="panel-collapse collapse">
@@ -30,8 +30,10 @@ import { NavNode } from '../models/navNode';
     `,
     styles: [`
         :host .nav.navbar-nav > li > a:hover { color: #22A7F0; }
+        :host .nav.navbar-nav ul  li.multiline { padding-bottom: 60px; }
         :host .nav.navbar-nav ul  li > a { height: auto; }
         :host .nav.navbar-nav ul  li > a > span.icon.fa { float: left; display: block; height: 14px; padding-top: 15px; }
+        :host .nav.navbar-nav ul  li > a > span.icon.fa.multiline { vertical-align: top; margin-top: 15px;  }
         :host .nav.navbar-nav ul  li > a > span.title { white-space: normal; padding-left: 38px; display: block; box-sizing: border-box; width: 100%; }
     `]
 })
